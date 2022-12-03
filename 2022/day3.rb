2a395a337adfc21e213baf854328552b758438c4
Shaun Kenny <skenny@gmail.com>
2022-12-03 12:06:22 -0400

    day 3 replaced [0] with .first

--- 2022/day3.rb
+++ 2022/day3.rb
@@ -5,14 +5,14 @@ input = File.read('2022/day3input.txt').split("\n")
 def part1(rucksacks)
     rucksacks.reduce(0) do |sum, rucksack|
         comp1, comp2 = rucksack.chars.each_slice(rucksack.size / 2).map(&:join)
-        duplicate = comp1.chars.intersection(comp2.chars)[0]
+        duplicate = comp1.chars.intersection(comp2.chars).first
         sum += @alpha.index(duplicate) + 1
     end
 end
 
 def part2(rucksacks)
     rucksacks.each_slice(3).reduce(0) do |sum, elf_group|
-        badge = elf_group[0].chars.intersection(elf_group[1].chars, elf_group[2].chars)[0]
+        badge = elf_group[0].chars.intersection(elf_group[1].chars, elf_group[2].chars).first
         sum += @alpha.index(badge) + 1
     end
 end
