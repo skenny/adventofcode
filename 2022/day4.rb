090a0135a0e97b0fed6235805838b6e0be262bc7
Shaun Kenny <skenny@gmail.com>
2022-12-04 12:09:39 -0400

    day 3 comment clarification

--- 2022/day4.rb
+++ 2022/day4.rb
@@ -8,7 +8,7 @@ def part2(assignment_pairs)
     assignment_pairs.count { |a, b| !a.to_a.intersection(b.to_a).empty? }
 end
 
-# "1-2,3-4" -> [1..2, 3..4]
+# "1-2,3-4" -> [[1,2], [3,4]] -> [1..2, 3..4]
 assignment_pairs = input.map { |pair| pair.split(",").map { |range_s| range_s.split("-").map(&:to_i) }.map { |b, e| b..e } }
 
 p part1(assignment_pairs)
