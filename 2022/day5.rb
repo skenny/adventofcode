88822d21df4ff4bad1bce7a88b1456a4dd967840
Shaun Kenny <skenny@gmail.com>
2022-12-05 21:12:19 -0400

    day 1 part 1

--- 2022/day5.rb
+++ 2022/day5.rb
@@ -1,10 +1,49 @@
 input = File.read("2022/day5input.txt").split("\n")
 
-def part1(input)
+def part1(stacks, steps)
+    steps.each do |num_crates, from, to|
+        (0...num_crates).each do |i|
+            stacks[to - 1].push(stacks[from - 1].pop)
+        end
+    end
+    stacks.map { |stack| stack.last }.join
 end
 
 def part2(input)
 end
 
-p part1(input)
-p part2(input)
+def parse_input(input)
+    stacks_lines = input.take_while { |line| line.length > 0 }
+    steps_lines = input.drop(stacks_lines.length + 1)
+
+    num_stacks = (stacks_lines[0].length + 1) / 4
+    stacks_lines.pop # remove stack numbers
+    stacks_lines.reverse!
+
+    stacks = []
+    (0...num_stacks).each do |col|
+        crate_idx = 1 + (col * 4)
+        stacks_lines.each do |line|
+            crate_val = line[crate_idx].strip
+            if !stacks[col]
+                stacks[col] = []
+            end
+            if crate_val.length > 0
+                stacks[col].push(crate_val)
+            end
+        end
+    end
+
+    steps = steps_lines.map do |step|
+        /move (\d+) from (\d+) to (\d+)/.match(step).captures.map(&:to_i)
+    end
+
+    [stacks, steps]
+end
+
+stacks, steps = parse_input(input)
+#p stacks
+#p steps
+
+puts part1(stacks, steps)
+puts part2(input)
