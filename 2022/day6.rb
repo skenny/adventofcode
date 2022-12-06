332abf328a958b3ee67979f27ec8e95022350546
Shaun Kenny <skenny@gmail.com>
2022-12-06 09:34:30 -0400

    day 6 part 1

--- 2022/day6.rb
+++ 2022/day6.rb
@@ -1,7 +1,11 @@
-input = File.read("2022/day6input.txt").split("\n")
+input = File.read("2022/day6input.txt")
 
 def part1(input)
-    0
+    i = 4
+    until input[i-4, 4].chars.uniq.length == 4 do
+        i += 1
+    end
+    i
 end
 
 def part2(input)
