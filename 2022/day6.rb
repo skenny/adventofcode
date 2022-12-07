53d9d481ad0aa2314cc912eeb345a47ee23c7bf0
Shaun Kenny <skenny@gmail.com>
2022-12-06 21:43:40 -0400

    day 6 cleanup

--- 2022/day6.rb
+++ 2022/day6.rb
@@ -1,8 +1,8 @@
 input = File.read("2022/day6input.txt")
 
-def scan_input(input, chunk_size)
+def scan_input(datastream, marker_len)
     i = 0
-    until input[i, chunk_size].chars.uniq.length == chunk_size do
+    until datastream[i, marker_len].chars.uniq.length == marker_len do
         i += 1
     end
     i
