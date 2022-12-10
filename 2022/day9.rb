d982c6f1874fbfb2cea8d1116f88ab93ae72f4e3
Shaun Kenny <skenny@gmail.com>
2022-12-09 21:20:29 -0400

    day 9 refactor

--- 2022/day9.rb
+++ 2022/day9.rb
@@ -1,81 +1,52 @@
 #input = "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2".split("\n")
 input = File.read("2022/day9input.txt").split("\n")
 
-def part1(motions)
-    head = [0,0]
-    tail = [0,0]
-    tail_visits = { [0,0] => true }
+@directions = {
+    "U" => [0, 1],
+    "D" => [0, -1],
+    "L" => [-1, 0],
+    "R" => [1, 0]
+}
+
+def play_rope(motions, num_knots)
+    knots = Array.new(num_knots) { [0, 0] }
+    tail_visits = { knots[0] => true }
 
     motions.each do |motion|
-        direction, amount = motion.split(" ")
-        amount.to_i.times do
-            head = move(head, direction)
-            tail = catch_up(head, tail)
-            tail_visits[tail] = true
-        end
-    end
-
-    tail_visits.keys.count
-end
-
-def part2(motions)
-    knots = Array.new(10) { |i| [0,0] }
-    tail_visits = { [0,0] => true }
-
-    motions.each do |motion|
-        direction, amount = motion.split(" ")
-        amount.to_i.times do
-            knots[0] = move(knots[0], direction)
-            (1..9).each do |knot_i|
-                knots[knot_i] = catch_up(knots[knot_i - 1], knots[knot_i])
+        direction, steps = motion.split(" ")
+        steps.to_i.times do
+            knots[0] = move(knots[0], @directions[direction])
+            (1...num_knots).each do |i|
+                knots[i] = catch_up(knots[i - 1], knots[i])
             end
-            tail_visits[knots[9]] = true
+            tail_visits[knots.last] = true
         end
     end
 
     tail_visits.keys.count
 end
 
-def move(knot, direction)
-    x, y = knot
-
-    case direction
-    when 'U'
-        [x, y+1]
-    when 'D'
-        [x, y-1]
-    when 'L'
-        [x-1, y]
-    when 'R'
-        [x+1, y]
-    else
-        puts "invalid direction " + direction
-        [x,y]
-    end
+def move(knot, vector)
+    [knot[0] + vector[0], knot[1] + vector[1]]
 end
 
 def catch_up(head, tail)
-    head_x, head_y = head
-    tail_x, tail_y = tail
+    delta_x = head[0] - tail[0]
+    delta_y = head[1] - tail[1]
 
-    catch_up_tail = (head_x-tail_x).abs > 1 || (head_y-tail_y).abs > 1
+    catch_up_tail = delta_x.abs > 1 || delta_y.abs > 1
 
     if catch_up_tail
-        delta_x = head_x - tail_x
         if (delta_x != 0)
-            tail_direction = delta_x > 0 ? "R" : "L"
-            tail_x, tail_y = move([tail_x, tail_y], tail_direction)
+            tail = move(tail, @directions[delta_x > 0 ? "R" : "L"])
         end
-
-        delta_y = head_y - tail_y
         if (delta_y != 0)
-            tail_direction = delta_y > 0 ? "U" : "D"
-            tail_x, tail_y = move([tail_x, tail_y], tail_direction)
+            tail = move(tail, @directions[delta_y > 0 ? "U" : "D"])
         end
     end
 
-    [tail_x, tail_y]
+    tail
 end
 
-puts part1(input)
-puts part2(input)
+puts play_rope(input, 2)
+puts play_rope(input, 10)
