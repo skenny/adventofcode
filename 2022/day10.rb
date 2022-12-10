61efaede34ec7cc7ca78c89985845cb92ab9ce6f
Shaun Kenny <skenny@gmail.com>
2022-12-10 10:57:42 -0400

    day 10 part 2

--- 2022/day10.rb
+++ 2022/day10.rb
@@ -2,11 +2,18 @@ input = ARGF.readlines
 
 def calculate_signal_strength(cycle, x)
     if [20, 60, 100, 140, 180, 220].include?(cycle)
-        signal_str = cycle * x
-        puts "cycle #{cycle} has signal strength of #{signal_str}"
-        signal_str
+        cycle * x
     else
         0
+    end
+end
+
+def draw_pixel!(pixels, cycle, x)
+    pixel_row_len = pixels[0].length
+    pixel_row = (cycle - 1) / pixel_row_len
+    pixel_pos = (cycle - 1) % pixel_row_len
+    if [x-1, x, x+1].include?(pixel_pos)
+        pixels[pixel_row][pixel_pos] = '#'
     end
 end
 
@@ -17,20 +24,44 @@ def part1(program)
     
     program.each do |instr|
         cmd, amt = instr.split(" ")
+        inc_x_amt = 0
         if cmd == "addx"
+            inc_x_amt = amt.to_i
             cycle += 1
             signal_strengths.push(calculate_signal_strength(cycle, x))
-            x += amt.to_i
         end
         cycle += 1
+        x += inc_x_amt
         signal_strengths.push(calculate_signal_strength(cycle, x))
     end
 
     signal_strengths.sum
 end
 
-def part2(input)
+def part2(program)
+    cycle = 1
+    x = 1
+    pixels = Array.new(6) { [" "] * 40 }
+
+    program.each do |instr|
+        cmd, amt = instr.split(" ")
+        inc_x_amt = 0
+
+        if cmd == "addx"
+            draw_pixel!(pixels, cycle, x)
+            inc_x_amt = amt.to_i
+            cycle += 1
+        end
+
+        draw_pixel!(pixels, cycle, x)
+        x += inc_x_amt
+        cycle += 1
+    end
+
+    pixels.each do |row|
+        puts row.join + "\n"
+    end
 end
 
 puts part1(input)
-puts part2(input)
+part2(input)
