a7986c338325fbb15b51462bb34e53760660e720
Shaun Kenny <skenny@gmail.com>
2022-12-11 11:39:30 -0400

    day 11 pt 2 setup

--- 2022/day11.rb
+++ 2022/day11.rb
@@ -8,7 +8,8 @@ class Monkey
     
     @@operations = {
         "+" => :+,
-        "*" => :*
+        "*" => :*,
+        "/" => :/
     }
 
     def initialize(items, operation, test)
@@ -20,23 +21,23 @@ class Monkey
 
     def inspect_items(monkeys)
         @items.each do |item|
-            puts "\tMonkey inspects an item with a worry level of #{item}."
+            #puts "\tMonkey inspects an item with a worry level of #{item}."
             
             operation_s, amount_s = @operation
             operation = @@operations[operation_s]
             amount = amount_s == "old" ? item : amount_s.to_i
             new_item = amount.send(operation, item)
-            puts "\t\tWorry level is #{operation_s} by #{amount_s} to #{new_item}."
+            #puts "\t\tWorry level is #{operation_s} by #{amount_s} to #{new_item}."
             
             new_item /= 3
-            puts "\t\tMonkey gets bored with item. Worry level is divided by 3 to #{new_item}."
+            #puts "\t\tMonkey gets bored with item. Worry level is divided by 3 to #{new_item}."
             
             divisor = @test[0].to_i
             test_result = new_item % divisor == 0
-            puts "\t\tCurrent worry level #{test_result ? 'is' : 'is not'} divisible by #{divisor}."
+            #puts "\t\tCurrent worry level #{test_result ? 'is' : 'is not'} divisible by #{divisor}."
 
             new_monkey = @test[test_result ? 1 : 2].to_i
-            puts "\t\tItem with worry level #{new_item} is thrown to monkey #{new_monkey}."
+            #puts "\t\tItem with worry level #{new_item} is thrown to monkey #{new_monkey}."
             
             monkeys[new_monkey].items.push(new_item)
         end
@@ -50,9 +51,9 @@ end
 
 def parse_input(input)
     input.map do |monkey_def|
-        monkey_def.split("\n").each do |monkey_def_line|
-            puts monkey_def_line + "\n"
-        end
+        # monkey_def.split("\n").each do |monkey_def_line|
+        #     puts monkey_def_line + "\n"
+        # end
 
         monkey_id, items_def, operation_def, test_def, test_true_def, test_false_def = monkey_def.split("\n")
         items = /Starting items: ([\d, ]*)/.match(items_def).captures[0].split(", ").map(&:to_i)
@@ -61,28 +62,47 @@ def parse_input(input)
         test_true = /If true: throw to monkey (\d)*/.match(test_true_def).captures[0]
         test_false = /If false: throw to monkey (\d)*/.match(test_false_def).captures[0]
         test = [test_divisor, test_true, test_false]
-        puts [items.to_s, operation.to_s, test.to_s].join(", ")
+        #puts [items.to_s, operation.to_s, test.to_s].join(", ")
 
         Monkey.new(items, operation, test)
     end
 end
 
-monkeys = parse_input(input)
-
 def play_round(monkeys)
-    monkeys.each_with_index do |monkey, i|
-        puts "Monkey #{i}:"
+    monkeys.each do |monkey|
         monkey.inspect_items(monkeys)
     end
 end
 
-20.times do |round|
-    play_round(monkeys)
+def part1(input)
+    monkeys = parse_input(input)
 
-    puts "After round #{round + 1}, the monkeys are holding items with these worry levels:"
-    monkeys.each_with_index do |monkey, i|
-        puts "Monkey #{i}: #{monkey.items.join(', ')}"
+    20.times do |round|
+        play_round(monkeys)
+
+        # puts "After round #{round + 1}, the monkeys are holding items with these worry levels:"
+        # monkeys.each_with_index do |monkey, i|
+        #     puts "Monkey #{i}: #{monkey.items.join(', ')}"
+        # end
     end
 
     puts monkeys.map(&:items_inspected).max(2).inject(:*)
 end
+
+def part2(input)
+    monkeys = parse_input(input)
+    
+    20.times do |round|
+        play_round(monkeys)
+
+        puts "== After round #{round + 1} =="
+        monkeys.each_with_index do |monkey, i|
+            puts "Monkey #{i} inspected items #{monkey.items_inspected} times."
+        end
+    end
+
+    puts monkeys.map(&:items_inspected).max(2).inject(:*)
+end
+
+part1(input)
+part2(input)
