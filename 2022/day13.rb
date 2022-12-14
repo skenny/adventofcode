6a482b81be8cf1babc8ebe59a567d8f81f06ffb9
Shaun Kenny <skenny@gmail.com>
2022-12-14 09:28:04 -0400

    day 13 removed debug

--- 2022/day13.rb
+++ 2022/day13.rb
@@ -14,56 +14,25 @@ def comp(a, b)
 end
 
 def comp_and_destroy(left, right)
-    debug = 0
-    if debug >= 1
-        puts "- Compare #{left} vs #{right}"
-    end
     loop do
         if left.empty? and right.empty?
-            if debug >= 2
-                puts "both sides are empty, moving on"
-            end
             return 0
         elsif left.empty? and not right.empty?
-            if debug >= 1
-                puts "- Left side ran out of items, so inputs are in the right order"
-            end
             return -1
         elsif right.empty?
-            if debug >= 1
-                puts "- Right side ran out of items, so inputs are not in the right order"
-            end
             return 1
         end
 
         l = left.shift
         r = right.shift
-        if debug >= 2
-            puts "popped l #{l}"
-            puts "popped r #{r}"
-        end
 
         l_is_array = Array.try_convert(l) != nil
         r_is_array = Array.try_convert(r) != nil
-        if debug >= 2
-            puts "l is array? #{l_is_array}"
-            puts "r is array? #{r_is_array}"
-        end
 
         if !l_is_array and !r_is_array
-            if debug >= 1
-                puts "- Compare #{l} vs #{r}"
-            end
-
             if l < r
-                if debug >= 1
-                    puts "- Left side is smaller, so inputs are in the right order"
-                end
                 return -1
             elsif r < l
-                if debug >= 1
-                    puts "- Right side is smaller, so inputs are not in the right order"
-                end
                 return 1
             else
                 if left.empty? and right.empty?
@@ -72,27 +41,13 @@ def comp_and_destroy(left, right)
             end
         else
             if !r_is_array
-                if debug >= 1
-                    puts "- Compare #{l} vs #{r}"
-                    puts "- Mixed types; convert right to [#{r}] and retry comparison"
-                end
                 r = [r]
             elsif !l_is_array
-                if debug >= 1
-                    puts "- Compare #{l} vs #{r}"
-                    puts "- Mixed types; convert left to [#{l}] and retry comparison"
-                end
                 l = [l]
             end
 
-            if debug >= 2
-                puts "- Left #{l} and right #{r} are arrays, recursing..."
-            end
             rez = comp_and_destroy(l, r)
             if rez != 0
-                if debug >= 2
-                    puts "- Arrays match, we are ordered!"
-                end
                 return rez
             end
         end
