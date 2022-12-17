cb0b2f632ab7a2beed0b6e0b7775e8715b77abc0
Shaun Kenny <skenny@gmail.com>
2022-12-16 21:58:42 -0400

    day 15 cleanup

--- 2022/day15.rb
+++ 2022/day15.rb
@@ -1,21 +1,22 @@
 input = ARGF.readlines
 
 class Sensor
+
     def initialize(loc_x, loc_y, beacon_x, beacon_y)
         @loc_x = loc_x
         @loc_y = loc_y
-    
-        # use the beacon coordinates to determine the sensor diamond "radius"
+        @radius = determine_radius(beacon_x, beacon_y)
+        @top = [@loc_x, @loc_y - @radius]
+        @bottom = [@loc_x, @loc_y + @radius]
+        @left = [@loc_x - @radius, @loc_y]
+        @right = [@loc_x + @radius, @loc_y]
+    end
 
-        d_x = beacon_x - @loc_x     # negative means beacon is left
-        d_y = beacon_y - @loc_y     # negative means beacon is above
-
-        # OPTIMIZATION - no need to step if the beacon is on an axis with the sensor, just use the deviation on the other axis
-
+    def determine_radius(beacon_x, beacon_y)
+        # step from beacon to an intersection with the sensor axis; the deviation is the sensor coverage diamond "radius"
         step_x = beacon_x < @loc_x ?  1 : beacon_x > @loc_x ? -1 : 0
         step_y = beacon_y < @loc_y ? -1 : beacon_y > @loc_y ?  1 : 0
 
-        # from d_x, d_y, use manhattan stepping to find a point of coverage
         at_x = beacon_x
         at_y = beacon_y
         while at_x != @loc_x and at_y != @loc_y do
@@ -23,17 +24,7 @@ class Sensor
             at_y += step_y
         end
 
-        # the radius is the distance from the sensor on either the x or y axis (whichever we hit)
-        if at_x == @loc_x
-            @radius = (at_y - @loc_y).abs
-        else
-            @radius = (at_x - @loc_x).abs
-        end
-
-        @top = [@loc_x, @loc_y - @radius]
-        @bottom = [@loc_x, @loc_y + @radius]
-        @left = [@loc_x - @radius, @loc_y]
-        @right = [@loc_x + @radius, @loc_y]
+        at_x == @loc_x ? (at_y - @loc_y).abs : (at_x - @loc_x).abs
     end
 
     def intersects(y_line)
@@ -48,6 +39,7 @@ class Sensor
             nil
         end
     end
+
 end
 
 def parse_input(input)
@@ -63,70 +55,60 @@ def parse_input(input)
     [sensors, beacons.uniq]
 end
 
-def count_coverage(ranges, bounds)
-    ranges.sort!
-    
-    coverage = 0
-    current_x = nil
-
-    while not ranges.empty? do
-        range = ranges.shift
-
-        range_start = range[0]
-        range_end = range[1]
-
-        if bounds
-            range_start = [range_start, bounds[0]].max
-            range_end = [range_end, bounds[1]].min
-        end
-
-        #puts "coverage is #{coverage}, current_x is #{current_x}, range is #{range_start}..#{range_end}"
-
-        if coverage == 0
-            #puts "starting!"
-            coverage += (range_end - range_start) + 1
-            current_x = range_end
-            next
-        end
-
-        # if this range is already covered, skip it
-        if range_end <= current_x
-            #puts "already covered"
-            next
-        end
-        
-        # if the next range starts before our current spot, add current -> end
-        if range_start < current_x
-            coverage += range_end - current_x
-            current_x = range_end
-            next
-        end
-
-        coverage += range_end - range_start
-        current_x = range_end
-    end
-
-    coverage
+def bound_range(range, bounds = nil)
+    [
+        bounds ? [range[0], bounds[0]].max : range[0], 
+        bounds ? [range[1], bounds[1]].min : range[1]
+    ]
 end
 
-def scan_line(sensors, beacons, test_line_y, bounds = nil)
-    #puts "testing line #{test_line_y}, and bounding by #{bounds}"
+def merge_ranges(ranges, bounds = nil)
+    sorted_ranges = ranges.sort
+    merged_ranges = []
+    merged_ranges.unshift(bound_range(sorted_ranges[0], bounds))
+
+    (1...sorted_ranges.size).each do |i|
+        range = bound_range(sorted_ranges[i], bounds)
+        working_range = merged_ranges.shift
+
+        if working_range[0] <= range[1] and working_range[1] >= range[0]
+            new_start = working_range[0]
+            new_end = [range[1], working_range[1]].max
+            merged_ranges.unshift([new_start, new_end])
+            next
+        end
+
+        merged_ranges.unshift(working_range)
+        merged_ranges.unshift(range)
+    end
+
+    merged_ranges.sort
+end
+
+def scan_line(sensors, beacons, test_line, bounds = nil)
     coverage_ranges = []
 
     sensors.each do |sensor|
-        if sensor.intersects(test_line_y)
-            coverage_range = sensor.coverage_range(test_line_y)
+        if sensor.intersects(test_line)
+            coverage_range = sensor.coverage_range(test_line)
             coverage_ranges.push(coverage_range) if coverage_range
         end
     end
 
-    count_coverage(coverage_ranges, bounds)
+    merge_ranges(coverage_ranges, bounds)
+end
+
+def count_coverage(ranges)
+    ranges.reduce(1) { |sum, range| sum + range[1] - range[0] }
 end
 
 def part1(sensors, beacons)
     a = Time.new.to_i * 1000
+
     test_line = 2_000_000
-    puts scan_line(sensors, beacons, test_line) - beacons.count { |beacon| beacon[1] == test_line }
+    coverage_ranges = scan_line(sensors, beacons, test_line)
+    puts count_coverage(coverage_ranges) - beacons.count { |beacon| beacon[1] == test_line }
+
     puts "part 1 took #{Time.new.to_i * 1000 - a}ms"
 end
 
@@ -136,20 +118,21 @@ def part2(sensors, beacons)
     distress_beacon = [0,0]
     bounds = [0, 4_000_000]
 
-    (0...4_000_000).each do |y|
-        coverage = scan_line(sensors, beacons, y, bounds)
-        if 4_000_000 - coverage == 1
-            puts "it's on line #{y}"
-            distress_beacon = [0, y]
+    (0...bounds[1]).each do |y|
+        coverage_ranges = scan_line(sensors, beacons, y, bounds)
+        coverage = count_coverage(coverage_ranges)
+        if bounds[1] - coverage == 1
+            x = coverage_ranges[0][1] + 1
+            distress_beacon = [x, y]
             break
         end
     end
 
-    puts "part 2 took #{Time.new.to_i * 1000 - a}ms"
+    puts 4_000_000 * distress_beacon[0] + distress_beacon[1]
 
-    4_000_000 * distress_beacon[0] + distress_beacon[1]
+    puts "part 2 took #{Time.new.to_i * 1000 - a}ms"
 end
 
 sensors, beacons = parse_input(input)
-puts part1(sensors, beacons)
-#puts part2(sensors, beacons)
+part1(sensors, beacons)
+part2(sensors, beacons)
