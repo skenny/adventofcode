79dddd764c695b9c96f0663eba5dcdc8547f4bd2
Shaun Kenny <skenny@gmail.com>
2022-12-20 11:22:40 -0400

    day 18

--- 2022/day18.rb
+++ 2022/day18.rb
@@ -1,11 +1,73 @@
-@face_directions = [
-    [1, 0, 0],
-    [-1, 0, 0],
-    [0, 1, 0],
-    [0, -1, 0],
-    [0, 0, 1],
-    [0, 0, -1]
-]
+class World
+    
+    @@face_directions = [
+        [1, 0, 0],
+        [-1, 0, 0],
+        [0, 1, 0],
+        [0, -1, 0],
+        [0, 0, 1],
+        [0, 0, -1]
+    ]
+
+    def initialize(x_max, y_max, z_max, fill)
+        @x_max = x_max + 1
+        @y_max = y_max + 1
+        @z_max = z_max + 1
+        @world = Array.new(@z_max) { Array.new(@y_max) { Array.new(@x_max, fill) } }
+    end
+
+    def fill(points, fill)
+        points.each { |point| set(point, fill) }
+    end
+
+    def flood_fill(points, fill)
+        while not points.empty?
+            next_points = []
+            points.each { |point|
+                set(point, fill)
+                next_neighbours = find_neighbours(point).select { |neighbour|
+                    is_in_bounds(neighbour) and get(neighbour) == :air
+                }
+                # I should just figure out how to use sets in ruby...
+                next_points = (next_points + next_neighbours).uniq
+            }
+            points = next_points
+        end
+    end
+        
+    def find_neighbours(point)
+        x, y, z = point
+        @@face_directions.map do |face_direction|
+            f_x, f_y, f_z = face_direction
+            [x + f_x, y + f_y, z + f_z]
+        end
+    end
+
+    def is_in_bounds(point)
+        x, y, z = point
+        (0...@x_max).include?(x) and (0...@y_max).include?(y) and (0...@z_max).include?(z)
+    end
+
+    def set(point, fill)
+        x, y, z = point
+        @world[z][y][x] = fill
+    end
+
+    def get(point)
+        x, y, z = point
+        @world[z][y][x]
+    end
+
+    def print
+        (0...@z_max).each do |z|
+            puts "Layer #{z}:"
+            (0...@y_max).each do |y|
+                puts "\t" + ("%02d" % y) + @world[z][y].map { |p| p == :air ? '.' : p == :water ? '~' : '#' }.join
+            end
+        end
+    end
+    
+end
 
 def build_world(droplets)
     # find the dimensions of the lava structure
@@ -19,85 +81,24 @@ def build_world(droplets)
         z_max = [z, z_max].max
     end
 
-    # create a 3d world array filled with air
-    world = Array.new(z_max + 2) { Array.new(y_max + 2) { Array.new(x_max + 2, :air) } }
-
-    # add the lava droplets
-    droplets.each do |droplet|
-        set(world, droplet, :lava)
-    end
-
-    # flood with water
-    flood_fill(world, [[0,0,0]], :water)
-
-    #print_world(world)
-
+    world = World.new(x_max, y_max, z_max, :air)
+    world.fill(droplets, :lava)
+    world.flood_fill([[0, 0, 0]], :water)
     world
-end
-
-def print_world(world)
-    world.each_with_index do |z_layer, z|
-        puts "Layer #{z}:"
-        z_layer.each do |y_column|
-            puts "\t" + y_column.map { |p| p == :air ? '.' : p == :water ? '~' : '#' }.join
-        end
-        puts ""
-    end
-end
-
-def flood_fill(world, points, fill)
-    while not points.empty?
-        next_points = []
-        points.each { |point|
-            set(world, point, fill)
-            next_neighbours = find_neighbours(world, point).select { |neighbour|
-                is_in_bounds(world, neighbour) and get(world, neighbour) == :air
-            }
-            # I should just figure out how to use sets in ruby...
-            next_points = (next_points + next_neighbours).uniq
-        }
-        points = next_points
-    end
-end
-
-def find_neighbours(world, point)
-    x, y, z = point
-    @face_directions.map do |face_direction|
-        f_x, f_y, f_z = face_direction
-        [x + f_x, y + f_y, z + f_z]
-    end
-end
-
-def is_in_bounds(world, point)
-    x, y, z = point
-    x_bounds = (0...world[0][0].size)
-    y_bounds = (0...world[0].size)
-    z_bounds = (0...world.size)
-    x_bounds.include?(x) and y_bounds.include?(y) and z_bounds.include?(z)
-end
-
-def get(world, point)
-    x, y, z = point
-    world[z][y][x]
-end
-
-def set(world, point, fill)
-    x, y, z = point
-    world[z][y][x] = fill
 end
 
 def part1(world, droplets)
     puts droplets.map { |droplet|
-        find_neighbours(world, droplet).count do |neighbour|
-            not is_in_bounds(world, neighbour) or get(world, neighbour) != :lava
+        world.find_neighbours(droplet).count do |neighbour|
+            not world.is_in_bounds(neighbour) or world.get(neighbour) != :lava
         end
     }.reduce(:+)
 end
 
 def part2(world, droplets)
     puts droplets.map { |droplet|
-        find_neighbours(world, droplet).count do |neighbour|
-            not is_in_bounds(world, neighbour) or get(world, neighbour) == :water
+        world.find_neighbours(droplet).count do |neighbour|
+            not world.is_in_bounds(neighbour) or world.get(neighbour) == :water
         end
     }.reduce(:+)
 end
