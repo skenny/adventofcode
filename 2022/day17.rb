cc7067a01c847b81055e598e9d7c3ebd0f15771c
Shaun Kenny <skenny@gmail.com>
2022-12-23 10:55:28 -0400

    day 17 progress

--- 2022/day17.rb
+++ 2022/day17.rb
@@ -1,12 +1,22 @@
 input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
 
-Point2d = Struct.new(:x, :y) do
+Point = Struct.new(:x, :y) do
     def plus(delta)
-        Point2d.new(x + delta.x, y + delta.y)
+        Point.new(x + delta.x, y + delta.y)
     end
 
     def to_s
         "[#{x},#{y}]"
+    end
+end
+
+Rock = Struct.new(:points) do
+    def apply(delta)
+        Rock.new(points.map { |p| p.plus(delta) })
+    end
+
+    def to_s
+        points.map(&:to_s).join(", ")
     end
 end
 
@@ -38,7 +48,7 @@ class RockGenerator
         (0...height).each do |y|
             (0...width).each do |x|
                 # [0,0] is bottom left
-                points.push(Point2d.new(x, height - y - 1)) if shape_arr[y][x] == "@"
+                points.push(Point.new(x, height - y - 1)) if shape_arr[y][x] == "@"
             end
         end
 
@@ -46,25 +56,14 @@ class RockGenerator
     end
 end
 
-class Rock
-    attr_accessor :points
-
-    def initialize(points)
-        @points = points
-    end
-
-    def apply(delta)
-        Rock.new(@points.map { |p| p.plus(delta) })
-    end
-end
-
 class Chamber
     @@chamber_width = 7
+    @@debug = false
 
     def initialize(jet_pattern)
         @jet_pattern = jet_pattern
         @jet_index = 0
-        @fill = (0...@@chamber_width).map { |x| Point2d.new(x, 0) } # fill the bottom row [x,0]
+        @fill = (0...@@chamber_width).map { |x| Point.new(x, 0) } # fill the bottom row [x,0]
     end
 
     def draw
@@ -73,41 +72,46 @@ class Chamber
     end
 
     def height
-        @fill.map { |x,y| y }.max || 0
+        @fill.map { |point| point.y }.max || 0
+    end
+
+    def in_bounds(rock)
+        rock.points.all? { |point| point.x > 0 and point.x < @@chamber_width}
     end
 
     def play_rock(rock)
-        puts "A new rock begins falling:"
-        rock = rock.apply(Point2d.new(2, height + 4))
+        puts "A new rock begins falling:" if @@debug
+        rock = rock.apply(Point.new(2, height + 4))
         step = 0
         loop do
+            puts "new step, rock is at #{rock}" if @@debug
             if step % 2 == 0
                 jet_direction = @jet_pattern[@jet_index]
                 @jet_index = (@jet_index + 1) % @jet_pattern.length
                 if jet_direction == "<"
-                    try_left = rock.apply(Point2d.new(-1, 0))
-                    if @fill.intersection(try_left.points).empty?
-                        puts "Jet of gas pushes rock left:"
+                    try_left = rock.apply(Point.new(-1, 0))
+                    if in_bounds(try_left) and @fill.intersection(try_left.points).empty?
+                        puts "Jet of gas pushes rock left:" if @@debug
                         rock = try_left
                     else 
-                        puts "Jet of gas pushes rock left, but nothing happens:"
+                        puts "Jet of gas pushes rock left, but nothing happens:" if @@debug
                     end
                 else
-                    try_right = rock.apply(Point2d.new(1, 0))
-                    if @fill.intersection(try_right.points).empty?
-                        puts "Jet of gas pushes rock right:"
+                    try_right = rock.apply(Point.new(1, 0))
+                    if in_bounds(try_right) and @fill.intersection(try_right.points).empty?
+                        puts "Jet of gas pushes rock right:" if @@debug
                         rock = try_right
                     else
-                        puts "Jet of gas pushes rock right, but nothing happens:"
+                        puts "Jet of gas pushes rock right, but nothing happens:" if @@debug
                     end
                 end
             else
-                try_down = rock.apply(Point2d.new(0, -1))
+                try_down = rock.apply(Point.new(0, -1))
                 if @fill.intersection(try_down.points).empty?
-                    puts "Rock falls 1 unit:"
+                    puts "Rock falls 1 unit:" if @@debug
                     rock = try_down
                 else
-                    puts "Rock falls 1 unit, causing it to come to rest:"
+                    puts "Rock falls 1 unit, causing it to come to rest:" if @@debug
                     @fill += rock.points
                     break
                 end
@@ -120,6 +124,8 @@ end
 chamber = Chamber.new(input)
 rock_generator = RockGenerator.new
 
-5.times do 
+2022.times do |i|
+    puts "Rock ##{i}..." if i % 500 == 0
     chamber.play_rock(rock_generator.next_rock)
 end
+puts chamber.height
