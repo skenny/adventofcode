8b2b0d4b7c68a3703023989148290e735c64816e
Shaun Kenny <skenny@gmail.com>
2022-12-25 13:54:43 -0400

    day 20 refactoring; real input fails

--- 2022/day20.rb
+++ 2022/day20.rb
@@ -16,14 +16,17 @@ class Node
     end
 end
 
-input = ARGF.read.split("\n").map(&:to_i)
-
-def part1(input)
-    nodes = input.map { |num| Node.new(num) }
+def parse_input(input)
+    nodes = input.map { |v| Node.new(v) }
     nodes.zip(nodes.rotate).each do |n1, n2|
         n1.next = n2
         n2.prev = n1
     end
+    nodes
+end
+
+def part1(input)
+    nodes = parse_input(input)
 
     # puts "before"
     # puts nodes.map(&:to_s)
@@ -35,13 +38,14 @@ def part1(input)
                 node.prev.next = node.next
                 node.next.prev = node.prev
 
+                # find target node
                 direction = node.sign
                 target = node
                 node.num.abs.times do
                     target = direction == -1 ? target.prev : target.next
                 end
 
-                # attach
+                # from target, determine new next/prev
                 if direction == -1
                     new_next = target
                     new_prev = new_next.prev
@@ -49,7 +53,10 @@ def part1(input)
                     new_prev = target
                     new_next = new_prev.next
                 end
+
                 # puts "#{node.num} moves between #{new_prev.num} and #{new_next.num}"
+
+                # re-attach
                 new_prev.next = node
                 new_next.prev = node
                 node.prev = new_prev
@@ -72,6 +79,11 @@ def part1(input)
         start = start.next
     end
     puts sum
+
+    # guesses...
+    # 13692 is too low
+    # 15128 is too high
 end
 
+input = ARGF.read.split("\n").map(&:to_i)
 part1(input)
