28d738aa7a28eac059e73c25a5c565b8f37dd988
Shaun Kenny <skenny@gmail.com>
2022-12-25 15:39:55 -0400

    day 20 part 2 but slow

--- 2022/day20.rb
+++ 2022/day20.rb
@@ -16,8 +16,8 @@ class Node
     end
 end
 
-def parse_input(input)
-    nodes = input.map { |v| Node.new(v) }
+def create_nodes(numbers)
+    nodes = numbers.map { |v| Node.new(v) }
     nodes.zip(nodes.rotate).each do |n1, n2|
         n1.next = n2
         n2.prev = n1
@@ -25,9 +25,7 @@ def parse_input(input)
     nodes
 end
 
-def part1(input)
-    nodes = parse_input(input)
-
+def mix(nodes, iterations=1)
     node_order = nodes.map do |node|
         [node.num, node]
     end
@@ -35,57 +33,69 @@ def part1(input)
     # puts "before"
     # puts nodes.map(&:to_s)
 
-    node_order.each do |num, node|
-        if num != 0
-            # detach
-            node.prev.next = node.next
-            node.next.prev = node.prev
+    iterations.times do
+        node_order.each do |num, node|
+            if num != 0
+                # detach
+                node.prev.next = node.next
+                node.next.prev = node.prev
 
-            # find target node
-            direction = node.sign
-            target = node
-            node.num.abs.times do
-                target = direction == -1 ? target.prev : target.next
+                # find target node
+                direction = node.sign
+                target = node
+                node.num.abs.times do
+                    target = direction == -1 ? target.prev : target.next
+                end
+
+                # from target, determine new next/prev
+                if direction == -1
+                    new_next = target
+                    new_prev = new_next.prev
+                else
+                    new_prev = target
+                    new_next = new_prev.next
+                end
+
+                # puts "#{node.num} moves between #{new_prev.num} and #{new_next.num}"
+
+                # re-attach
+                new_prev.next = node
+                new_next.prev = node
+                node.prev = new_prev
+                node.next = new_next
             end
-
-            # from target, determine new next/prev
-            if direction == -1
-                new_next = target
-                new_prev = new_next.prev
-            else
-                new_prev = target
-                new_next = new_prev.next
-            end
-
-            # puts "#{node.num} moves between #{new_prev.num} and #{new_next.num}"
-
-            # re-attach
-            new_prev.next = node
-            new_next.prev = node
-            node.prev = new_prev
-            node.next = new_next
         end
     end
 
     # puts "after"
     # puts nodes.map(&:to_s)
+end
 
-    # TODO use start to walk nodes and build an array?
+def find_grove_coordinates(nodes)
     start = nodes.find { |node| node.num == 0 }
-    sum = 0
+    grove_coordinates = []
     3001.times do |i|
         if i > 0 and i % 1000 == 0
-            # puts "#{i} is #{start.num}"
-            sum += start.num
+            grove_coordinates.push(start.num)
         end
         start = start.next
     end
-    puts sum
-
-    # guesses...
-    # 13692 is too low
-    # 15128 is too high
+    grove_coordinates
 end
 
-input = ARGF.read.split("\n").map(&:to_i)
-part1(input)
+def part1(numbers)
+    nodes = create_nodes(numbers)
+    mix(nodes)
+    puts find_grove_coordinates(nodes).sum
+end
+
+def part2(numbers)
+    decryption_key = 811589153
+    nodes = create_nodes(numbers.map { |n| n * decryption_key })
+    mix(nodes, 10)
+    puts find_grove_coordinates(nodes).sum
+end
+
+numbers = ARGF.read.split("\n").map(&:to_i)
+part1(numbers)
+#part2(numbers)
