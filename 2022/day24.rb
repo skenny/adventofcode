dbfaf123e2e4f97582aa9e7d12f8863193e54804
Shaun Kenny <skenny@gmail.com>
2022-12-26 15:54:53 -0400

    day 24 - works for test but slow for real

--- 2022/day24.rb
+++ 2022/day24.rb
@@ -1,28 +1,4 @@
-Point = Struct.new(:x, :y) do
-    def plus(delta)
-        Point.new(x + delta.x, y + delta.y)
-    end
-
-    def neighbours
-        [[-1, 0], [1, 0], [0, -1], [0, 1]].map { |v_x, v_y| Point.new(x + v_x, y + v_y) }
-    end
-
-    def inspect
-        "[#{x},#{y}]"
-    end
-
-    def to_s
-        "[#{x},#{y}]"
-    end
-end
-
-class Player
-    attr_accessor :point
-
-    def initialize(point)
-        @point = point
-    end
-end
+require 'set'
 
 class Blizzard
     attr_accessor :point, :direction_vector
@@ -31,13 +7,13 @@ class Blizzard
         @point = point
         case direction
         when "^"
-            @direction_vector = Point.new(0, -1)
+            @direction_vector = [0, -1]
         when "v"
-            @direction_vector = Point.new(0, 1)
+            @direction_vector = [0, 1]
         when "<"
-            @direction_vector = Point.new(-1, 0)
+            @direction_vector = [-1, 0]
         when ">"
-            @direction_vector = Point.new(1, 0)
+            @direction_vector = [1, 0]
         else
             "Invalid blizzard direction #{direction}!"
             exit
@@ -46,24 +22,23 @@ class Blizzard
 end
 
 class Grid
-    attr_accessor :start, :target
+    attr_accessor :entrance, :exit, :blizzards
 
     def initialize(input)
         @width = input[0].size
         @height = input.size
+        @entrance = nil
         @exit = nil
-        @player = nil
         @blizzards = []
-        @lookup = {}
 
         blizzard_contents = ["<", ">", "v", "^"]
 
         input.each_with_index do |row, y|
             row.chars.each_with_index do |col, x|
-                point = Point.new(x, y)
+                point = [x, y]
 
                 if y == 0 and col == '.'
-                    @player = Player.new(point)
+                    @entrance = point
                 elsif y == input.size-1 and col == '.'
                     @exit = point
                 end
@@ -71,26 +46,71 @@ class Grid
                 if blizzard_contents.include?(col)
                     @blizzards.push(Blizzard.new(point, col))
                 end
-
-                @lookup[point] = col
             end
         end
+    end
 
-        @lookup[@player.point] = 'E'
+    def step_blizzards
+        @blizzards.each do |blizzard|
+            x, y = blizzard.point
+            n_x = x + blizzard.direction_vector[0]
+            n_y = y + blizzard.direction_vector[1]
+            if not in_bounds([n_x, n_y])
+                if n_x == 0
+                    n_x = @width - 2
+                elsif n_x == @width - 1
+                    n_x = 1
+                elsif n_y == 0
+                    n_y = @height - 2
+                elsif n_y == @height - 1
+                    n_y = 1
+                end
+            end
+            blizzard.point = [n_x, n_y]
+        end
     end
 
     def in_bounds(p)
-        p.x > 0 and p.x < @width and p.y > 0 and p.y < @height
-    end
-
-    def get(p)
-        @lookup[p]
+        p[0] > 0 and p[0] < @width - 1 and p[1] > 0 and p[1] < @height - 1
     end
 
 end
 
+def search(grid, start, target)
+    to_check = Set.new
+    to_check.add(start)
+    minute = 0
+
+    while true
+        puts "Minute #{minute}, checking #{to_check.size}..."
+
+        grid.step_blizzards
+        next_blizzard_state = grid.blizzards.map(&:point)
+
+        next_to_check = Set.new
+        to_check.each do |current|
+            if current == target
+                return minute
+            end
+            x, y = current
+            possible_next = [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]].select { |neighbour| grid.in_bounds(neighbour) and not next_blizzard_state.include?(neighbour) }
+            if possible_next.empty?
+                next_to_check.add(current)
+            else
+                next_to_check += possible_next
+            end
+        end
+
+        to_check = next_to_check
+        minute += 1
+    end
+end
+
 def part1(input)
     grid = Grid.new(input)
+    puts "Starting at #{Time.new}..."
+    puts search(grid, grid.entrance, grid.exit)
+    puts "Finished at #{Time.new}"
 end
 
 input = ARGF.read.split("\n")
