b255e96b024417ac935c7673dc4f4c479369c885
Shaun Kenny <skenny@gmail.com>
2022-12-28 10:57:30 -0400

    day 19 setup

--- 2022/day19.rb
+++ 2022/day19.rb
@@ -1,26 +1,5 @@
-class Blueprint
-    attr_accessor :id
-    attr_accessor :ore_robot_ore_cost
-    attr_accessor :clay_robot_ore_cost
-    attr_accessor :obsidian_robot_ore_cost
-    attr_accessor :obsidian_robot_clay_cost
-    attr_accessor :geode_robot_ore_cost
-    attr_accessor :geode_robot_obsidian_cost
-
-    def initialize(blueprint_parts)
-        @id = blueprint_parts[0]
-        @ore_robot_ore_cost = blueprint_parts[1]
-        @clay_robot_ore_cost = blueprint_parts[2]
-        @obsidian_robot_ore_cost = blueprint_parts[3]
-        @obsidian_robot_clay_cost = blueprint_parts[4]
-        @geode_robot_ore_cost = blueprint_parts[5]
-        @geode_robot_obsidian_cost = blueprint_parts[6]
-    end
-
-    def to_s
-        "Blueprint #{@id}: ore=[#{@ore_robot_ore_cost} ore], clay=[#{@clay_robot_ore_cost} ore], obsidian=[#{@obsidian_robot_ore_cost} ore, #{@obsidian_robot_clay_cost} clay], geode=[#{@geode_robot_ore_cost} ore, #{@geode_robot_obsidian_cost} obsidian]"
-    end
-end
+Blueprint = Struct.new(:id, :ore_robot_ore_cost, :clay_robot_ore_cost, :obsidian_robot_ore_cost, :obsidian_robot_clay_cost, :geode_robot_ore_cost, :geode_robot_obsidian_cost)
+State = Struct.new(:minute, :ore, :clay, :obsidian, :geodes, :ore_robots, :clay_robots, :obsidian_robots, :geode_robots)
 
 def parse_input(input)
     input.map do |blueprint_spec|
@@ -28,17 +7,18 @@ def parse_input(input)
             .match(blueprint_spec)
             .captures
             .map(&:to_i)
-        Blueprint.new(blueprint_parts)
+        Blueprint.new(*blueprint_parts)
     end
 end
 
-def crack_geodes(blueprint, time_allowed=24)
+def crack_geodes(blueprint, state, time_allowed=24)
+    # TODO (haha)
     0
 end
 
 def part1(input)
     blueprints = parse_input(input)
-    quality_levels = blueprints.map { |blueprint| blueprint.id * crack_geodes(blueprint) }
+    quality_levels = blueprints.map { |blueprint| blueprint.id * crack_geodes(blueprint, State.new(1, 0, 0, 0, 0, 1, 0, 0, 0)) }
     puts quality_levels.sum
 end
 
