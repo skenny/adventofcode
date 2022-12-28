5908992d7782cfcdcb2f466bba82e0ffa794a7b5
Shaun Kenny <skenny@gmail.com>
2022-12-28 11:29:55 -0400

    day 19 part 1 first attempt

--- 2022/day19.rb
+++ 2022/day19.rb
@@ -11,14 +11,45 @@ def parse_input(input)
     end
 end
 
-def crack_geodes(blueprint, state, time_allowed=24)
-    # TODO (haha)
-    0
+def crack_geodes(blueprint, states_to_try, time_allowed=24)
+    results = states_to_try.each do |state|
+        if state.minute > time_allowed
+            next
+        end
+
+        new_ore = state.ore + state.ore_robots
+        new_clay = state.clay + state.clay_robots
+        new_obsidian = state.obsidian + state.obsidian_robots
+        new_geodes = state.geodes + state.geode_robots
+
+        next_minute = state.minute + 1
+        next_states = []
+        next_states.push(State.new(next_minute, new_ore, new_clay, new_obsidian, new_geodes, state.ore_robots, state.clay_robots, state.obsidian_robots, state.geode_robots))
+
+        # possible optimizations:
+        # - once we have a geode robot, only try the state where we buy another geode robot
+
+        if new_ore >= blueprint.ore_robot_ore_cost
+            next_states.push(State.new(next_minute, new_ore - blueprint.ore_robot_ore_cost, new_clay, new_obsidian, new_geodes, state.ore_robots + 1, state.clay_robots, state.obsidian_robots, state.geode_robots))
+        end
+        if new_ore >= blueprint.clay_robot_ore_cost
+            next_states.push(State.new(next_minute, new_ore - blueprint.clay_robot_ore_cost, new_clay, new_obsidian, new_geodes, state.ore_robots, state.clay_robots + 1, state.obsidian_robots, state.geode_robots))
+        end
+        if new_ore >= blueprint.obsidian_robot_ore_cost and new_clay >= blueprint.obsidian_robot_clay_cost
+            next_states.push(State.new(next_minute, new_ore - blueprint.obsidian_robot_ore_cost, new_clay - blueprint.obsidian_robot_clay_cost, new_obsidian, new_geodes, state.ore_robots, state.clay_robots, state.obsidian_robots + 1, state.geode_robots))
+        end
+        if new_ore >= blueprint.geode_robot_ore_cost and new_obsidian >= blueprint.geode_robot_obsidian_cost
+            next_states.push(State.new(next_minute, new_ore - blueprint.geode_robot_ore_cost, new_clay, new_obsidian - blueprint.geode_robot_obsidian_cost, new_geodes, state.ore_robots, state.clay_robots, state.obsidian_robots, state.geode_robots + 1))
+        end
+
+        crack_geodes(blueprint, next_states, time_allowed)
+    end
+    results.map(&:geodes).max
 end
 
 def part1(input)
     blueprints = parse_input(input)
-    quality_levels = blueprints.map { |blueprint| blueprint.id * crack_geodes(blueprint, State.new(1, 0, 0, 0, 0, 1, 0, 0, 0)) }
+    quality_levels = blueprints.map { |blueprint| blueprint.id * crack_geodes(blueprint, [State.new(1, 0, 0, 0, 0, 1, 0, 0, 0)]) }
     puts quality_levels.sum
 end
 
