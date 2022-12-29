0174de8398bfa318d347ef091391e32bdb84970e
Shaun Kenny <skenny@gmail.com>
2022-12-29 11:18:12 -0400

    day 19 readability changes

--- 2022/day19.rb
+++ 2022/day19.rb
@@ -18,6 +18,10 @@ def crack_geodes(blueprint, time_allowed=24)
     finished_paths = []
     minute = 0
 
+    max_orebots_needed = [blueprint.ore_robot_ore_cost, blueprint.obsidian_robot_ore_cost, blueprint.geode_robot_ore_cost].max
+    max_claybots_needed = blueprint.obsidian_robot_clay_cost
+    max_obsidibots_needed = blueprint.geode_robot_obsidian_cost
+
     while not states.empty?
         puts "\tMinute #{minute}, #{states.size} state(s)..."
         next_states = Set.new
@@ -39,16 +43,21 @@ def crack_geodes(blueprint, time_allowed=24)
 
             # TODO once we have a geode robot, only try the state where we buy another geode robot?
 
-            if state.ore >= blueprint.ore_robot_ore_cost and state.ore_robots < [blueprint.ore_robot_ore_cost, blueprint.obsidian_robot_ore_cost, blueprint.geode_robot_ore_cost].max
+            can_buy_orebot = state.ore >= blueprint.ore_robot_ore_cost
+            can_buy_claybot = state.ore >= blueprint.clay_robot_ore_cost
+            can_buy_obsidibot = state.ore >= blueprint.obsidian_robot_ore_cost && state.clay >= blueprint.obsidian_robot_clay_cost
+            can_buy_geodebot = state.ore >= blueprint.geode_robot_ore_cost && state.obsidian >= blueprint.geode_robot_obsidian_cost
+
+            if can_buy_orebot and state.ore_robots < max_orebots_needed
                 next_states.add(State.new(next_minute, new_ore - blueprint.ore_robot_ore_cost, new_clay, new_obsidian, new_geodes, state.ore_robots + 1, state.clay_robots, state.obsidian_robots, state.geode_robots))
             end
-            if state.ore >= blueprint.clay_robot_ore_cost and state.clay_robots < blueprint.obsidian_robot_clay_cost
+            if can_buy_claybot and state.clay_robots < max_claybots_needed
                 next_states.add(State.new(next_minute, new_ore - blueprint.clay_robot_ore_cost, new_clay, new_obsidian, new_geodes, state.ore_robots, state.clay_robots + 1, state.obsidian_robots, state.geode_robots))
             end
-            if state.ore >= blueprint.obsidian_robot_ore_cost and state.clay >= blueprint.obsidian_robot_clay_cost and state.obsidian_robots < blueprint.geode_robot_obsidian_cost
+            if can_buy_obsidibot and state.obsidian_robots < max_obsidibots_needed
                 next_states.add(State.new(next_minute, new_ore - blueprint.obsidian_robot_ore_cost, new_clay - blueprint.obsidian_robot_clay_cost, new_obsidian, new_geodes, state.ore_robots, state.clay_robots, state.obsidian_robots + 1, state.geode_robots))
             end
-            if state.ore >= blueprint.geode_robot_ore_cost and state.obsidian >= blueprint.geode_robot_obsidian_cost
+            if can_buy_geodebot
                 next_states.add(State.new(next_minute, new_ore - blueprint.geode_robot_ore_cost, new_clay, new_obsidian - blueprint.geode_robot_obsidian_cost, new_geodes, state.ore_robots, state.clay_robots, state.obsidian_robots, state.geode_robots + 1))
             end
 
@@ -89,5 +98,5 @@ def part2(input)
 end
 
 input = ARGF.read.split("\n")
-#part1(input)
-part2(input)
+part1(input)
+#part2(input)
