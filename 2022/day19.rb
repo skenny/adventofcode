b68d48173455389554da1ec6d664fedcf0f20d9c
Shaun Kenny <skenny@gmail.com>
2022-12-29 13:01:47 -0400

    day 19 reorganization to reduce work

--- 2022/day19.rb
+++ 2022/day19.rb
@@ -28,41 +28,41 @@ def crack_geodes(blueprint, time_allowed=24)
 
         states.each do |state|
             next_minute = state.minute + 1
-            time_remaining = time_allowed - next_minute
-
-            if next_minute > time_allowed
-                finished_paths.push(state)
-                next
-            end
 
             new_ore = state.ore + state.ore_robots
             new_clay = state.clay + state.clay_robots
             new_obsidian = state.obsidian + state.obsidian_robots
             new_geodes = state.geodes + state.geode_robots
 
-            next_states.add(State.new(next_minute, new_ore, new_clay, new_obsidian, new_geodes, state.ore_robots, state.clay_robots, state.obsidian_robots, state.geode_robots))
+            new_resources_state = State.new(next_minute, new_ore, new_clay, new_obsidian, new_geodes, state.ore_robots, state.clay_robots, state.obsidian_robots, state.geode_robots)
 
-            if time_remaining > 0
-                if time_remaining > 1 && state.ore_robots < max_orebots_needed
-                    if state.ore >= blueprint.ore_robot_ore_cost
-                        next_states.add(State.new(next_minute, new_ore - blueprint.ore_robot_ore_cost, new_clay, new_obsidian, new_geodes, state.ore_robots + 1, state.clay_robots, state.obsidian_robots, state.geode_robots))
-                    end
-                end
+            if next_minute == time_allowed
+                finished_paths.push(new_resources_state)
+            else
+                next_states.add(new_resources_state)
 
-                if time_remaining > 1 && state.clay_robots < max_claybots_needed
-                    if state.ore >= blueprint.clay_robot_ore_cost
-                        next_states.add(State.new(next_minute, new_ore - blueprint.clay_robot_ore_cost, new_clay, new_obsidian, new_geodes, state.ore_robots, state.clay_robots + 1, state.obsidian_robots, state.geode_robots))
-                    end
-                end
-
-                if time_remaining > 1 && state.obsidian_robots < max_obsidibots_needed
-                    if state.ore >= blueprint.obsidian_robot_ore_cost && state.clay >= blueprint.obsidian_robot_clay_cost
-                        next_states.add(State.new(next_minute, new_ore - blueprint.obsidian_robot_ore_cost, new_clay - blueprint.obsidian_robot_clay_cost, new_obsidian, new_geodes, state.ore_robots, state.clay_robots, state.obsidian_robots + 1, state.geode_robots))
-                    end
-                end
-
+                # always buy a geode bot if we can
                 if state.ore >= blueprint.geode_robot_ore_cost && state.obsidian >= blueprint.geode_robot_obsidian_cost
                     next_states.add(State.new(next_minute, new_ore - blueprint.geode_robot_ore_cost, new_clay, new_obsidian - blueprint.geode_robot_obsidian_cost, new_geodes, state.ore_robots, state.clay_robots, state.obsidian_robots, state.geode_robots + 1))
+                end
+
+                # only buy a non geode bot if we have enough time to use it and also possibly build a geode bot (e.g. 2+ mins left)
+                if time_allowed - next_minute > 1
+                    if state.ore_robots < max_orebots_needed
+                        if state.ore >= blueprint.ore_robot_ore_cost
+                            next_states.add(State.new(next_minute, new_ore - blueprint.ore_robot_ore_cost, new_clay, new_obsidian, new_geodes, state.ore_robots + 1, state.clay_robots, state.obsidian_robots, state.geode_robots))
+                        end
+                    end
+                    if state.clay_robots < max_claybots_needed
+                        if state.ore >= blueprint.clay_robot_ore_cost
+                            next_states.add(State.new(next_minute, new_ore - blueprint.clay_robot_ore_cost, new_clay, new_obsidian, new_geodes, state.ore_robots, state.clay_robots + 1, state.obsidian_robots, state.geode_robots))
+                        end
+                    end
+                    if state.obsidian_robots < max_obsidibots_needed
+                        if state.ore >= blueprint.obsidian_robot_ore_cost && state.clay >= blueprint.obsidian_robot_clay_cost
+                            next_states.add(State.new(next_minute, new_ore - blueprint.obsidian_robot_ore_cost, new_clay - blueprint.obsidian_robot_clay_cost, new_obsidian, new_geodes, state.ore_robots, state.clay_robots, state.obsidian_robots + 1, state.geode_robots))
+                        end
+                    end
                 end
             end
         end
