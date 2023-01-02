c07b14659c3ebe97f0b528256482008f1afd1f95
Shaun Kenny <skenny@gmail.com>
2023-01-02 14:01:19 -0400

    day 22 p1

--- 2022/day22.rb
+++ 2022/day22.rb
@@ -1,6 +1,126 @@
-Point = Struct.new(:x, :y, :z)
+Point = Struct.new(:x, :y) do
 
-class World
+    def move(v, w, h)
+        Point.new((x + v.x) % w, (y + v.y) % h)
+    end
+
+    def turn_right
+        if x == 1 and y == 0
+            Point.new(0, 1)
+        elsif x == 0 and y == 1
+            Point.new(-1, 0)
+        elsif x == -1 and y == 0
+            Point.new(0, -1)
+        else
+            Point.new(1, 0)
+        end
+    end
+
+    def turn_left
+        if x == 1 and y == 0
+            Point.new(0, -1)
+        elsif x == 0 and y == -1
+            Point.new(-1, 0)
+        elsif x == -1 and y == 0
+            Point.new(0, 1)
+        else
+            Point.new(1, 0)
+        end
+    end
+
+    def to_s
+        "[#{x},#{y}]"
+    end
+end
+
+class World1
+
+    def initialize(grid_spec)
+        @height = grid_spec.size
+        @grid_sections = grid_spec.each { |line| line.chars }
+        @player_position = Point.new(@grid_sections[0].chars.index { |char| char != ' ' }, 0)
+        @facing_vector = Point.new(1, 0)
+    end
+
+    def get_final_password
+        row = @player_position.y + 1
+        col = @player_position.x + 1
+        facing_val, facing_dir = get_facing
+        1000 * row + 4 * col + facing_val
+    end
+
+    def get_facing
+        case @facing_vector
+        when Point.new(1, 0)  # right
+            [0, ">"]
+        when Point.new(0, 1)  # down
+            [1, "v"]
+        when Point.new(-1, 0) # left
+            [2, "<"]
+        when Point.new(0, -1) # up
+            [3, "^"]
+        else
+            puts "Invalid facing vector: #{@facing_vector}"
+            [-1, "_"]
+        end
+    end
+
+    def grid_position(position)
+        @grid_sections[position.y][position.x]
+    end
+
+    def move_player(instr)
+        if /(\d+)/.match?(instr)
+            steps = instr.to_i
+            #puts "moving #{steps} from #{@player_position}"
+            steps.times do |step_i|
+                next_position = get_next_player_position
+                grid_contents = grid_position(next_position)
+                #puts "in front at #{next_position} is #{grid_contents}"
+                if grid_contents == '#'
+                    #puts "stopping!"
+                    break
+                end
+                @player_position = next_position
+            end
+        else
+            turn_player(instr)
+        end
+    end
+
+    def turn_player(direction)
+        #puts "turning #{direction} from #{get_facing}"
+        case direction
+        when "L"
+            @facing_vector = @facing_vector.turn_left
+        when "R"
+            @facing_vector = @facing_vector.turn_right
+        end
+        #puts "\tnow facing #{get_facing}"
+    end
+
+    def get_next_player_position
+        front = @player_position.move(@facing_vector, @grid_sections[@player_position.y].size, @height)
+        grid_contents = grid_position(front) || ' '
+        if grid_contents == ' '
+            facing_val, facing_dir = get_facing
+            case facing_dir
+            when ">" # right
+                Point.new(@grid_sections[front.y].chars.index { |char| char != ' ' }, front.y)
+            when "v" # down
+                Point.new(front.x, @grid_sections.index { |row| front.x < row.size && row[front.x] != ' ' })
+            when "<" # left
+                Point.new(@grid_sections[front.y].chars.rindex { |char| char != ' ' }, front.y)
+            when "^" # up
+                Point.new(front.x, @grid_sections.rindex { |row| front.x < row.size && row[front.x] != ' ' })
+            end
+        else
+            front
+        end
+    end
+end
+
+class World2
 
     def initialize(grid_spec)
         @grid_sections = parse_grid_sections(grid_spec)
@@ -20,20 +140,15 @@ class World
         sections
     end
 
-    def move_player(move)
-    end
-end
-
-def parse_moves(moves_spec)
-    moves_spec.scan(/\d+|\D+/)
 end
 
 def part1(grid_spec, moves_spec)
-    world = World.new(grid_spec)
-    moves = parse_moves(moves_spec)
+    world = World1.new(grid_spec)
+    moves = moves_spec.scan(/\d+|\D+/)
     moves.each do |move|
         world.move_player(move)
     end
+    puts world.get_final_password
 end
 
 grid_spec, moves_spec = ARGF.read.split("\n\n").map{ |section_lines| section_lines.split("\n") }
