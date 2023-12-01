c2e1230a0288a8a1a793060f566d73e9cbda0cf6
Shaun Kenny <skenny@gmail.com>
2023-12-01 11:01:25 -0400

    2023: util.ReadFile()

--- 2023/util/util.go
+++ 2023/util/util.go
@@ -1,5 +1,15 @@
 package util
 
-func ReadFile() string {
-	return "ReadFile()"
+import (
+	"os"
+	"strings"
+)
+
+func ReadFile(filename string) ([]string, error) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(string(content), "\n")
+	return lines, nil
 }
