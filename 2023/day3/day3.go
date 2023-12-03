4e8e7b01d8de17ca5a01570ee60cb1a939d846a4
Shaun Kenny <skenny@gmail.com>
2023-12-03 14:01:41 -0400

    reworked algo; day 3 part 2 done

--- 2023/day3/day3.go
+++ 2023/day3/day3.go
@@ -16,6 +16,11 @@ type Point struct {
 	Y int
 }
 
+type PartNumber struct {
+	Number int
+	Points []Point
+}
+
 func main() {
 	fmt.Printf("Day %v\n", day)
 	input := util.ReadInput(day)
@@ -24,16 +29,7 @@ func main() {
 }
 
 func part1(input []string) {
-	// find all of the symbols
-	symbols := make(map[Point]bool)
-	for y, row := range input {
-		for x, ch := range row {
-			if !unicode.IsNumber(ch) && ch != '.' {
-				symbols[Point{x, y}] = true
-			}
-		}
-	}
-	//fmt.Printf("Symbols are at %v\n", symbols)
+	symbols, partNumbers := parseInput(input)
 
 	// adjacent point offsets to check
 	adjacentOffsets := []Point{
@@ -42,67 +38,119 @@ func part1(input []string) {
 		{-1, 1}, {0, 1}, {1, 1},
 	}
 
-	// keep track of points we've looked at, so we can skip the following digit points for numbers we've already evaluated
+	sumValidPartNumbers := 0
+	for _, partNumber := range partNumbers {
+		valid := false
+		for _, occupyingPoint := range partNumber.Points {
+			for _, offsetPoint := range adjacentOffsets {
+				checkPoint := Point{occupyingPoint.X + offsetPoint.X, occupyingPoint.Y + offsetPoint.Y}
+				_, found := symbols[checkPoint]
+				if found {
+					valid = true
+					break
+				}
+			}
+			if valid {
+				break
+			}
+		}
+		if valid {
+			sumValidPartNumbers += partNumber.Number
+		}
+	}
+
+	fmt.Printf("Part 1: %v\n", sumValidPartNumbers)
+}
+
+func part2(input []string) {
+	symbols, partNumbers := parseInput(input)
+
+	// adjacent point offsets to check
+	adjacentOffsets := []Point{
+		{-1, -1}, {0, -1}, {1, -1},
+		{-1, 0}, {1, 0},
+		{-1, 1}, {0, 1}, {1, 1},
+	}
+
+	const gear = "*"
+	sumGearRatios := 0
+
+	for symbolPoint, symbol := range symbols {
+		if symbol == gear {
+			touchingPartNumbers := []int{}
+			for _, partNumber := range partNumbers {
+				touching := false
+				for _, partNumberPoint := range partNumber.Points {
+					for _, offsetPoint := range adjacentOffsets {
+						checkPoint := Point{partNumberPoint.X + offsetPoint.X, partNumberPoint.Y + offsetPoint.Y}
+						if checkPoint == symbolPoint {
+							touching = true
+							break
+						}
+					}
+					if touching {
+						break
+					}
+				}
+				if touching {
+					touchingPartNumbers = append(touchingPartNumbers, partNumber.Number)
+				}
+			}
+			if len(touchingPartNumbers) == 2 {
+				sumGearRatios += (touchingPartNumbers[0] * touchingPartNumbers[1])
+			}
+		}
+	}
+
+	fmt.Printf("Part 2: %v\n", sumGearRatios)
+}
+
+func parseInput(input []string) (map[Point]string, []PartNumber) {
+	symbols := make(map[Point]string)
+	partNumbers := []PartNumber{}
 	checkedPoints := make(map[Point]bool)
 
-	validPartNumbers := []int{}
-
-	// walk through and evaluate each number, checking if it touches a symbol
 	for y, row := range input {
 		for x, ch := range row {
 			currentPoint := Point{x, y}
+
 			if checkedPoints[currentPoint] {
 				continue
 			}
 
+			checkedPoints[currentPoint] = true
+
 			if unicode.IsNumber(ch) {
-				numberArr := []string{string(ch)}
-				checkedPoints[currentPoint] = true
+				occupyingPoints := []Point{currentPoint}
+				partNumberArr := []string{string(ch)}
 
 				// check if the number continues to the right
 				for nextX := currentPoint.X + 1; nextX < len(row); nextX++ {
+					nextPoint := Point{nextX, y}
+
 					adjacentRune := rune(row[nextX])
 					if unicode.IsNumber(adjacentRune) {
-						numberArr = append(numberArr, string(adjacentRune))
-						checkedPoints[Point{nextX, y}] = true
+						occupyingPoints = append(occupyingPoints, nextPoint)
+						partNumberArr = append(partNumberArr, string(adjacentRune))
+						checkedPoints[nextPoint] = true
 					} else {
 						break
 					}
 				}
 
-				partNumberStr := strings.Join(numberArr[:], "")
+				partNumberStr := strings.Join(partNumberArr[:], "")
+				partNumber, err := strconv.Atoi(partNumberStr)
 
-				// check if any adjacent point has a symbol
-				valid := false
-				for i := range partNumberStr {
-					for _, offsetPoint := range adjacentOffsets {
-						checkPoint := Point{x + i + offsetPoint.X, y + offsetPoint.Y}
-						if symbols[checkPoint] {
-							partNumber, err := strconv.Atoi(partNumberStr)
-							if err != nil {
-								panic(fmt.Sprintf("unable to convert part number %v to an int: %v", partNumberStr, err))
-							}
-							//fmt.Printf("%v at %v is a valid part number! touches symbol at %v\n", partNumber, currentPoint, checkPoint)
-							validPartNumbers = append(validPartNumbers, partNumber)
-							valid = true
-							break
-						}
-					}
-
-					if valid {
-						break
-					}
+				if err != nil {
+					panic(fmt.Sprintf("unable to convert part number %v to an int: %v\n", partNumberStr, err))
 				}
 
-				if !valid {
-					fmt.Printf("%v at %v is not a valid part number\n", partNumberStr, currentPoint)
-				}
+				partNumbers = append(partNumbers, PartNumber{partNumber, occupyingPoints})
+			} else if ch != '.' {
+				symbols[currentPoint] = string(ch)
 			}
 		}
 	}
 
-	fmt.Printf("Part 1: %v\n", util.SumInts(validPartNumbers))
-}
-
-func part2(input []string) {
+	return symbols, partNumbers
 }
