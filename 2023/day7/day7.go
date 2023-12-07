e11b400d40bca8353f45cd8db5bd9ef532af8b07
Shaun Kenny <skenny@gmail.com>
2023-12-07 16:20:51 -0400

    day 7 part 2 not working

--- 2023/day7/day7.go
+++ 2023/day7/day7.go
@@ -17,7 +17,15 @@ type Hand struct {
 	Wager int
 }
 
-var Cards = strings.Split("AKQJT98765432", "")
+var Cards = strings.Split("AKQJT98765432J", "")
+
+const FIVE_OF_A_KIND = 1
+const FOUR_OF_A_KIND = 2
+const FULL_HOUSE = 3
+const THREE_OF_A_KIND = 4
+const TWO_PAIR = 5
+const ONE_PAIR = 6
+const HIGH_CARD = 7
 
 func main() {
 	fmt.Printf("Day %v\n", day)
@@ -28,19 +36,52 @@ func main() {
 }
 
 func test() {
-	hands := []string{"AAAAA", "AA8AA", "23332", "TTT98", "23432", "A23A4", "23456"}
-	for _, hand := range hands {
-		fmt.Printf("%v scores %v\n", hand, scoreHand(Hand{strings.Split(hand, ""), 1}))
+	//hands := []string{"AAAAA", "AA8AA", "23332", "TTT98", "23432", "A23A4", "23456"}
+	jokerHands := []string{"JAAAA", "JAAA5", "JAA55", "JAA45", "JA345", "JJAAA", "JJAA5", "JJA45", "JJJAA", "JJJA5", "JJJJA"}
+	for _, hand := range jokerHands {
+		fmt.Printf("%v scores %v\n", hand, scoreHand(Hand{strings.Split(hand, ""), 1}, true))
 	}
 }
 
 func part1(input []string) {
+	const jokersAreWild = false
 	hands := parseInput(input)
+	fmt.Printf("Part 1: %v\n", calculateTotalWinnings(hands, jokersAreWild))
+}
+
+func part2(input []string) {
+	const jokersAreWild = true
+	hands := parseInput(input)
+	fmt.Printf("Part 2: %v\n", calculateTotalWinnings(hands, jokersAreWild))
+}
+
+func parseInput(input []string) []Hand {
+	hands := []Hand{}
+	for _, line := range input {
+		parts := strings.Fields(line)
+		cards := strings.Split(parts[0], "")
+		wager := util.MustAtoi(parts[1])
+		hands = append(hands, Hand{cards, wager})
+	}
+	return hands
+}
+
+func calculateTotalWinnings(hands []Hand, jokersAreWild bool) int {
+	sortedHands := sortHands(hands, jokersAreWild)
+	numHands := len(sortedHands)
+	totalWinnings := 0
+	for i, hand := range sortedHands {
+		totalWinnings += (numHands - i) * hand.Wager
+	}
+	return totalWinnings
+}
+
+func sortHands(hands []Hand, jokersAreWild bool) []Hand {
 	sort.Slice(hands, func(i, j int) bool {
 		hand1 := hands[i]
 		hand2 := hands[j]
-		hand1Score := scoreHand(hand1)
-		hand2Score := scoreHand(hand2)
+		hand1Score := scoreHand(hand1, jokersAreWild)
+		hand2Score := scoreHand(hand2, jokersAreWild)
 
 		if hand1Score != hand2Score {
 			return hand1Score < hand2Score
@@ -58,30 +99,10 @@ func part1(input []string) {
 		return false
 	})
 
-	numHands := len(hands)
-	totalWinnings := 0
-	for i, hand := range hands {
-		totalWinnings += (numHands - i) * hand.Wager
-	}
-
-	fmt.Printf("Part 1: %v", totalWinnings)
-}
-
-func part2(input []string) {
-}
-
-func parseInput(input []string) []Hand {
-	hands := []Hand{}
-	for _, line := range input {
-		parts := strings.Fields(line)
-		cards := strings.Split(parts[0], "")
-		wager := util.MustAtoi(parts[1])
-		hands = append(hands, Hand{cards, wager})
-	}
 	return hands
 }
 
-func scoreHand(hand Hand) int {
+func scoreHand(hand Hand, jokersAreWild bool) int {
 	cardCounts := make(map[string]int)
 	for _, card := range hand.Cards {
 		cardCount, ok := cardCounts[card]
@@ -91,37 +112,78 @@ func scoreHand(hand Hand) int {
 		cardCounts[card] = cardCount + 1
 	}
 
+	jokers := 0
+	if jokersAreWild {
+		jokers = cardCounts["J"]
+	}
+
+	var countValues []int = maps.Values(cardCounts)
+	slices.Sort(countValues)
+
 	switch len(cardCounts) {
 	case 1:
-		// 5 of a kind
-		return 1
+		return FIVE_OF_A_KIND
 	case 2:
-		var countValues []int = maps.Values(cardCounts)
-		slices.Sort(countValues)
+		fourOfAKind := countValues[0] == 1 && countValues[1] == 4
+		fullHouse := countValues[0] == 2 && countValues[1] == 3
 
-		if countValues[1] == 4 {
-			// 4 of a kind
-			return 2
+		if jokers > 0 {
+			return FIVE_OF_A_KIND
 		}
 
-		// full house
-		return 3
+		if fourOfAKind {
+			return FOUR_OF_A_KIND
+		}
+
+		if fullHouse {
+			return FULL_HOUSE
+		}
+
+		panic(fmt.Sprintf("unexpected cards %v", hand.Cards))
 	case 3:
-		var countValues []int = maps.Values(cardCounts)
-		slices.Sort(countValues)
+		threeAndOneAndOne := countValues[0] == 1 && countValues[1] == 1 && countValues[2] == 3
+		twoPairAndOne := countValues[0] == 1 && countValues[1] == 2 && countValues[2] == 2
 
-		if countValues[2] == 3 {
-			// 3 of a kind
-			return 4
+		if threeAndOneAndOne {
+			if jokers == 3 {
+				return FOUR_OF_A_KIND
+			}
+			if jokers == 1 {
+				return FOUR_OF_A_KIND
+			}
+			return THREE_OF_A_KIND
 		}
-		// 3 of a kind, 2 pair
-		return 5
+
+		if twoPairAndOne {
+			if jokers == 2 {
+				return FOUR_OF_A_KIND
+			}
+			if jokers == 1 {
+				return FULL_HOUSE
+			}
+			return TWO_PAIR
+		}
+
+		panic(fmt.Sprintf("unexpected cards %v", hand.Cards))
 	case 4:
-		// pair
-		return 6
+		one_pair := countValues[0] == 1 && countValues[1] == 1 && countValues[2] == 1 && countValues[3] == 2
+
+		if one_pair {
+			if jokers == 2 {
+				return THREE_OF_A_KIND
+			}
+			if jokers == 1 {
+				return THREE_OF_A_KIND
+			}
+			return ONE_PAIR
+		}
+
+		panic(fmt.Sprintf("unexpected cards %v", hand.Cards))
 	case 5:
-		// high card
-		return 7
+		if jokers == 1 {
+			return ONE_PAIR
+		}
+		return HIGH_CARD
 	default:
 		panic(fmt.Sprintf("hand score is 0: %v", hand))
 	}
