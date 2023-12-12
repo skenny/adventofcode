34ff9625f10189df67f14bda73a0507070d432ff
Shaun Kenny <skenny@gmail.com>
2023-12-11 23:51:43 -0400

    day 10 cleanup

--- 2023/day11/day11.go
+++ 2023/day11/day11.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"strings"
 
 	"github.com/skenny/adventofcode/2023/util"
@@ -15,103 +14,46 @@ type Vertex struct {
 	Y int
 }
 
-type VertexPair struct {
-	Vertex1 Vertex
-	Vertex2 Vertex
-}
-
 func main() {
 	fmt.Printf("Day %v\n", day)
-	input := util.ReadInput(day)
-	part1(input)
-	part2(input)
+	grid := parseInput(util.ReadInput(day))
+	fmt.Printf("Part 1: %v\n", solveTheUniverse(grid, 2))
+	fmt.Printf("Part 2: %v\n", solveTheUniverse(grid, 1000000))
 }
 
-func part1(input []string) {
-	grid := parseInput(input)
-	fmt.Printf("Part 1: %v\n", findShortestDistances(grid, 2))
-}
-
-func part2(input []string) {
-	grid := parseInput(input)
-	fmt.Printf("Part 2: %v\n", findShortestDistances(grid, 1000000))
-}
-
-func findShortestDistances(grid [][]string, expansionAmount int) int {
-	emptyRows, emptyCols := findEmptyRowsAndCols(grid)
-	galaxies := findGalaxies(grid)
-	galaxyPairs := findGalaxyPairs(galaxies)
-	shortestDistances := util.MapSlice(galaxyPairs, func(galaxyPair VertexPair) int {
-		xDistance := int(math.Abs(float64(galaxyPair.Vertex2.X - galaxyPair.Vertex1.X)))
-		yDistance := int(math.Abs(float64(galaxyPair.Vertex2.Y - galaxyPair.Vertex1.Y)))
-
-		// for each vertex pair, count the number of empty rows and cols between them, and add in the expansion distance factor for each dimension
-
-		minX := int(math.Min(float64(galaxyPair.Vertex1.X), float64(galaxyPair.Vertex2.X)))
-		maxX := int(math.Max(float64(galaxyPair.Vertex1.X), float64(galaxyPair.Vertex2.X)))
-		xExpansionFactor := 0
-		for _, x := range emptyCols {
-			if x >= minX && x <= maxX {
-				xExpansionFactor += 1
-			}
-		}
-
-		minY := int(math.Min(float64(galaxyPair.Vertex1.Y), float64(galaxyPair.Vertex2.Y)))
-		maxY := int(math.Max(float64(galaxyPair.Vertex1.Y), float64(galaxyPair.Vertex2.Y)))
-		yExpansionFactor := 0
-		for _, y := range emptyRows {
-			if y >= minY && y <= maxY {
-				yExpansionFactor += 1
-			}
-		}
-
-		return xDistance + (xExpansionFactor * (expansionAmount - 1)) + yDistance + (yExpansionFactor * (expansionAmount - 1))
-	})
-
-	return util.SumInts(shortestDistances)
-}
-
-func findGalaxyPairs(galaxies []Vertex) []VertexPair {
-	galaxyPairs := []VertexPair{}
-	for i := 0; i < len(galaxies); i++ {
-		for j := i + 1; j < len(galaxies); j++ {
-			galaxyPairs = append(galaxyPairs, VertexPair{galaxies[i], galaxies[j]})
-		}
-	}
-	return galaxyPairs
-}
-
-func findGalaxies(grid [][]string) []Vertex {
+func solveTheUniverse(grid [][]string, expandBy int) int {
+	emptyRows := []int{}
+	emptyCols := []int{}
 	galaxies := []Vertex{}
 	for y, row := range grid {
+		if rowEmpty(y, grid) {
+			emptyRows = append(emptyRows, y)
+		}
 		for x, cell := range row {
 			if cell == "#" {
 				galaxies = append(galaxies, Vertex{x, y})
 			}
+			if y == 0 && colEmpty(x, grid) {
+				emptyCols = append(emptyCols, x)
+			}
 		}
 	}
-	return galaxies
+
+	distances := []int{}
+	for i := 0; i < len(galaxies); i++ {
+		for j := i + 1; j < len(galaxies); j++ {
+			galaxy1, galaxy2 := galaxies[i], galaxies[j]
+			manhattanDistance := abs(galaxy2.X-galaxy1.X) + abs(galaxy2.Y-galaxy1.Y)
+			xExpansion := countInRange(emptyCols, min(galaxy1.X, galaxy2.X), max(galaxy1.X, galaxy2.X))
+			yExpansion := countInRange(emptyRows, min(galaxy1.Y, galaxy2.Y), max(galaxy1.Y, galaxy2.Y))
+			distances = append(distances, manhattanDistance+(xExpansion*(expandBy-1))+(yExpansion*(expandBy-1)))
+		}
+	}
+
+	return util.SumInts(distances)
 }
 
-func findEmptyRowsAndCols(grid [][]string) (rows []int, cols []int) {
-	emptyRows := []int{}
-	for y := 0; y < len(grid); y++ {
-		if isRowEmpty(y, grid) {
-			emptyRows = append(emptyRows, y)
-		}
-	}
-
-	emptyCols := []int{}
-	for x := 0; x < len(grid[0]); x++ {
-		if isColEmpty(x, grid) {
-			emptyCols = append(emptyCols, x)
-		}
-	}
-
-	return emptyRows, emptyCols
-}
-
-func isRowEmpty(row int, grid [][]string) bool {
+func rowEmpty(row int, grid [][]string) bool {
 	rowIsEmpty := true
 	for _, cell := range grid[row] {
 		rowIsEmpty = rowIsEmpty && cell == "."
@@ -119,12 +61,43 @@ func isRowEmpty(row int, grid [][]string) bool {
 	return rowIsEmpty
 }
 
-func isColEmpty(col int, grid [][]string) bool {
+func colEmpty(col int, grid [][]string) bool {
 	colIsEmpty := true
 	for _, row := range grid {
 		colIsEmpty = colIsEmpty && row[col] == "."
 	}
 	return colIsEmpty
+}
+
+func countInRange(vals []int, min, max int) int {
+	count := 0
+	for _, v := range vals {
+		if v >= min && v <= max {
+			count += 1
+		}
+	}
+	return count
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
+func min(v1, v2 int) int {
+	if v1 < v2 {
+		return v1
+	}
+	return v2
+}
+
+func max(v1, v2 int) int {
+	if v1 > v2 {
+		return v1
+	}
+	return v2
 }
 
 func parseInput(input []string) [][]string {
