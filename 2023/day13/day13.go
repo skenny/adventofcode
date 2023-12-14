4455b3c7ce0952ceae43d0b8b2719fe1b7c78b60
Shaun Kenny <skenny@gmail.com>
2023-12-14 16:06:58 -0400

    day 13 part 2

--- 2023/day13/day13.go
+++ 2023/day13/day13.go
@@ -23,61 +23,77 @@ func (p Pattern) rasterize() {
 func main() {
 	fmt.Printf("Day %v\n", day)
 	input := util.ReadInput(day)
-	part1(input)
+	//part1(input)
 	part2(input)
 }
 
 func part1(input []string) {
 	patterns := parseInput(input)
+	fmt.Printf("Part 1: %v\n", summarize(patterns, 0))
+}
+
+func part2(input []string) {
+	patterns := parseInput(input)
+	fmt.Printf("Part 2: %v\n", summarize(patterns, 1))
+	// 38286 is too high
+	// 37972 is too low
+}
+
+func summarize(patterns []Pattern, smudgeAmount int) int {
 	lefts := 0
 	aboves := 0
 	for i, pattern := range patterns {
 		//pattern.rasterize()
-		h1, h2 := findMirror(pattern.Rows)
-		hmf := h1 != -1 && h2 != -1
-		if hmf {
-			//fmt.Printf("Horizontal mirror between %v, %v\n", h1+1, h2+1)
+		h1, hfound := findMirror(pattern.Rows, smudgeAmount)
+		if hfound {
 			aboves += h1 + 1
-		}
-		v1, v2 := findMirror(pattern.Cols)
-		vmf := v1 != -1 && v2 != -1
-		if vmf {
-			//fmt.Printf("Vertical mirror between %v, %v\n", v1+1, v2+1)
-			lefts += v1 + 1
-		}
-		if !hmf && !vmf {
-			panic(fmt.Sprintf("no mirror found in pattern %v", i))
+		} else {
+			v1, vfound := findMirror(pattern.Cols, smudgeAmount)
+			if vfound {
+				lefts += v1 + 1
+			} else {
+				panic(fmt.Sprintf("no mirror found in pattern %v", i))
+			}
 		}
 	}
-	fmt.Printf("Part 1: %v\n", lefts+(aboves*100))
+	return lefts + (aboves * 100)
 }
 
-func part2(input []string) {
-}
-
-func findMirror(lines []string) (int, int) {
+func findMirror(lines []string, smudgeAmount int) (int, bool) {
 	for i := 0; i < len(lines)-1; i++ {
 		left := lines[i]
 		right := lines[i+1]
-		if left == right {
+		smudges := difference(left, right)
+		if smudges <= smudgeAmount {
 			for x := 1; x < len(lines); x++ {
 				lx := i - x
 				lr := i + 1 + x
 				if lx >= 0 && lr < len(lines) {
 					left = lines[lx]
 					right = lines[lr]
-					if left != right {
+					smudges += difference(left, right)
+					if smudges > smudgeAmount {
 						// no match, not the mirror line
 						break
 					}
-				} else {
+				} else if smudges == smudgeAmount {
 					// out of bounds... must be the mirror line
-					return i, i + 1
+					return i, true
 				}
 			}
 		}
 	}
-	return -1, -1
+	return -1, false
+}
+
+func difference(s1, s2 string) int {
+	difference := 0
+	for i := 0; i < len(s1); i++ {
+		if s1[i] != s2[i] {
+			difference += 1
+		}
+	}
+	return difference
 }
 
 func rowsToCols(rows []string) []string {
