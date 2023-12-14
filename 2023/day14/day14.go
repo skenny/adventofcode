77e92f621b6f51b140bdf70e8192c2dc55596f4d
Shaun Kenny <skenny@gmail.com>
2023-12-14 10:26:00 -0400

    day 14 part 2 naive; might not work properly

--- 2023/day14/day14.go
+++ 2023/day14/day14.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/skenny/adventofcode/2023/util"
 )
@@ -11,7 +12,7 @@ const day int = 14
 
 func main() {
 	fmt.Printf("Day %v\n", day)
-	input := util.ReadInput(day)
+	input := util.ReadTestInput(day)
 	part1(input)
 	part2(input)
 }
@@ -22,6 +23,32 @@ func part1(input []string) {
 }
 
 func part2(input []string) {
+	columns := parseColumns(input)
+	for i := 0; i < 1_000_000_000; i++ {
+		debug := i%100_000 == 0
+		if debug {
+			fmt.Printf("pass %v\n", i)
+		}
+		for j := 0; j < 4; j++ {
+			columns = rotateCounterclockwise(util.MapSlice(columns, func(column string) string { return tilt(column) }))
+		}
+	}
+	fmt.Printf("Part 2: %v\n", util.SumInts(util.MapSlice(columns, func(column string) int { return calculateLoad(column) })))
+}
+
+func rotateCounterclockwise(columns []string) []string {
+	return util.MapSlice(columns, rotate)
+}
+
+func rotate(column string) string {
+	size := len(column)
+	buf := make([]byte, size)
+	for start := 0; start < size; {
+		r, n := utf8.DecodeRuneInString(column[start:])
+		start += n
+		utf8.EncodeRune(buf[size-start:], r)
+	}
+	return string(buf)
 }
 
 func calculateLoad(column string) int {
