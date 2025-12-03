139297e1650c57becba49bbaa8f2d87641679b18
Shaun Kenny <skenny@gmail.com>
2025-12-02 20:42:07 -0400

    day 2 part 2

--- 2025/day2.py
+++ 2025/day2.py
@@ -21,8 +21,25 @@ def part1():
     print(f"Part 1: {sum(invalid_ids)}")
 
 def part2():
-    answer = 0
-    print(f"Part 2: {answer}")
+    invalid_ids = []
+    for id_range in input:
+        for id in range(id_range[0], id_range[1] + 1):
+            id_str = str(id)
+            id_str_len = len(id_str)
+            #print(f"id {id}, length {id_str_len}")
+            sz = id_str_len // 2
+            while sz > 0:
+                if id_str_len % sz == 0:
+                    #print(f"\tdividing into {id_str_len // sz} chunks of size {sz}")
+                    chunks = [ id_str[i:i + sz] for i in range(0, id_str_len, sz) ]
+                    #print(f"\tgot {chunks}")
+                    if len(set(chunks)) == 1:
+                        #print(f"\t{id} is invalid!")
+                        invalid_ids.append(id)
+                        break
+                sz -= 1
+
+    print(f"Part 2: {sum(invalid_ids)}")
 
 part1()
 part2()
