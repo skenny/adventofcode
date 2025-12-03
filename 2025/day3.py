00c5b5adf9bf863411fbbb097ba472cbad0ea213
Shaun Kenny <skenny@gmail.com>
2025-12-03 14:00:28 -0400

    day 3 part 1

--- 2025/day3.py
+++ 2025/day3.py
@@ -1,10 +1,17 @@
 input = []
-with open('input/day3-test.txt', 'r') as input_file:
+with open('input/day3.txt', 'r') as input_file:
     input = input_file.readlines()
 
 def part1():
-    output = 0
-    print(f"Part 1: {output}")
+    joltages = []
+    for battery_bank in input:
+        max_joltage = 0
+        for i in range(0, len(battery_bank) - 1):
+            for j in range(i + 1, len(battery_bank)):
+                joltage = int(battery_bank[i] + battery_bank[j])
+                max_joltage = max(joltage, max_joltage)
+        joltages.append(max_joltage)
+    print(f"Part 1: {sum(joltages)}")
 
 def part2():
     output = 0
