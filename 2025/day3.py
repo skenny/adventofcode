3eb17fc02565853208aa60578784f1d3bfd08113
Shaun Kenny <skenny@gmail.com>
2025-12-04 10:42:57 -0400

    day 3 part 2

--- 2025/day3.py
+++ 2025/day3.py
@@ -1,6 +1,6 @@
 input = []
 with open('input/day3.txt', 'r') as input_file:
-    input = input_file.readlines()
+    input = [v.strip() for v in input_file.readlines()]
 
 def part1():
     joltages = []
@@ -14,8 +14,25 @@ def part1():
     print(f"Part 1: {sum(joltages)}")
 
 def part2():
-    output = 0
-    print(f"Part 2: {output}")
+    joltages = []
+    for battery_bank_str in input:
+        #print(f"considering bank {battery_bank_str}")
+        battery_bank = list(map(int, list(battery_bank_str)))
+        active_batteries = []
+        i = 0
+        while len(active_batteries) < 12:
+            num_batteries_to_keep = 12 - len(active_batteries) - 1
+            batteries_to_consider = battery_bank[i:len(battery_bank) - num_batteries_to_keep]
+            #print(f"\tkeeping {batteries_to_keep}; considering {batteries_to_consider} out of {battery_bank}...")
+            max_battery = max(batteries_to_consider)
+            max_battery_i = batteries_to_consider.index(max_battery)
+            active_batteries.append(max_battery)
+            #print(f"\tmax battery is {max_battery} at {max_battery_i}; active batteries is {active_batteries}")
+            i += max_battery_i + 1
+        max_joltage = int("".join(map(str, active_batteries)))
+        #print(f"\tmax joltage is {max_joltage}")
+        joltages.append(max_joltage)
+    print(f"Part 2: {sum(joltages)}")
 
 part1()
 part2()
