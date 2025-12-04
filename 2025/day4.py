f9303136fb4e106ab897053c684866d14b61a373
Shaun Kenny <skenny@gmail.com>
2025-12-04 13:20:39 -0400

    stubbed out day 4

---
+++ 2025/day4.py
@@ -0,0 +1,12 @@
+input = []
+with open('input/day3.txt', 'r') as input_file:
+    input = [v.strip() for v in input_file.readlines()]
+
+def part1():
+    print("Part 1:")
+
+def part2():
+    print("Part 2:")
+
+part1()
+part2()
