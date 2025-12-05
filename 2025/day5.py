74acf8e42a3dcd56e475bdb6e5788f728fe848dc
Shaun Kenny <skenny@gmail.com>
2025-12-05 17:00:17 -0400

    day 5 part 2

--- 2025/day5.py
+++ 2025/day5.py
@@ -14,7 +14,7 @@ def parse_input(input):
             continue
         if pre_split:
             low, high = line.split('-')
-            fresh_ingredient_ranges.append(range(int(low), int(high) + 1))
+            fresh_ingredient_ranges.append([int(low), int(high)])
         else:
             available_ingredients.append(int(line))
     return fresh_ingredient_ranges, available_ingredients
@@ -26,7 +26,7 @@ def part1(fresh_ingredient_ranges, available_ingredients):
         is_fresh = False
         for fresh_ingredient_range in fresh_ingredient_ranges:
             #print(f"\tchecking in {fresh_ingredient_range}")
-            if ingredient in fresh_ingredient_range:
+            if ingredient >= fresh_ingredient_range[0] and ingredient <= fresh_ingredient_range[1]:
                 #print(f"\tis fresh!")
                 is_fresh = True
                 break
@@ -34,10 +34,46 @@ def part1(fresh_ingredient_ranges, available_ingredients):
             fresh_count += 1
     print(f"Part 1: {fresh_count}")
 
-def part2():
-    print("Part 2:")
+def part2(fresh_ingredient_ranges):
+    sorted_fresh_ingredient_ranges = sorted(fresh_ingredient_ranges, key = lambda r: r[0])
+    fresh_ingredient_count = 0
+    ranges_seen = []
+
+    for fresh_ingredient_range in sorted_fresh_ingredient_ranges:
+        range_start = fresh_ingredient_range[0]
+        range_end = fresh_ingredient_range[1]
+        range_len = range_end - range_start + 1
+        #print(f"{fresh_ingredient_range}: start is {range_start} and stop is {range_end} and len is {range_len}")
+
+        skip = False
+
+        for seen_range in ranges_seen:
+            #print(f"\tcomparing with seen range {seen_range}")
+
+            if range_start >= seen_range[0] and range_end <= seen_range[1]:
+                #print("\t\tcompletely contained! ignoring...")
+                skip = True
+                break
+
+            if range_start >= seen_range[0] and range_start <= seen_range[1]:
+                range_start = seen_range[1] + 1
+                #print(f"\t\tnew range starts within seen range; adjusting range_start to {range_start}")
+            elif range_end >= seen_range[0] and range_end <= seen_range[1]:
+                range_end = seen_range[0] - 1
+                #print(f"\t\tnew range ends within seen range; adjusting range_end to {range_start}")
+
+        if not skip:
+            adjusted_range_ingredient_count = range_end - range_start + 1
+            #print(f"\tadjusted range: {range_start} to {range_end}, len is {adjusted_range_ingredient_count}")
+
+            fresh_ingredient_count += adjusted_range_ingredient_count
+            #print(f"\tfresh_ingredient_count is {fresh_ingredient_count}")
+
+        ranges_seen.append(fresh_ingredient_range)
+
+    print(f"Part 2: {fresh_ingredient_count}")
 
 fresh_ingredient_ranges, available_ingredients = parse_input(read_input())
 
 part1(fresh_ingredient_ranges, available_ingredients)
-part2()
+part2(fresh_ingredient_ranges)
