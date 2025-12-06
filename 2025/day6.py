92e96e62684d2f7f8a1dbe69d496c83b0c505083
Shaun Kenny <skenny@gmail.com>
2025-12-06 18:39:26 -0400

    day 6 part 2

--- 2025/day6.py
+++ 2025/day6.py
@@ -1,9 +1,10 @@
 import math
+import re
 
 def read_input():
     input = []
-    with open('input/day6.txt', 'r') as input_file:
-        input = [v.strip() for v in input_file.readlines()]
+    with open('input/day6-test.txt', 'r') as input_file:
+        input = [v for v in input_file.readlines()]
     return input
 
 def part1(input):
@@ -20,7 +21,35 @@ def part1(input):
     print(f"Part 1: {total}")
 
 def part2(input):
-    print("Part 2:")
+    ops_line = input[-1]
+    ops_padded = re.findall(r"([*+]\s+)", ops_line)
+
+    totals = []
+    offset = 0
+
+    for i, op in enumerate(ops_padded):
+        # remove the extra space (the divider between columns) after every operator except the last one
+        if i != len(ops_padded) - 1:
+            op = op[:-1]
+        
+        col_width = len(op)
+        nums = list(map(lambda l: l[offset:offset+col_width], input[:-1]))
+        offset += col_width + 1
+        
+        nums_t = []
+        for i in range(col_width - 1, -1, -1):
+            digits = []
+            for num in nums:
+                digits.append(num[i])
+            nums_t.append(int("".join(digits)))
+
+        if op.strip() == '*':
+            totals.append(math.prod(nums_t))
+        else:
+            totals.append(sum(nums_t))
+
+    total = sum(totals)
+    print(f"Part 2: {total}")
     
 input = read_input()
 part1(input)
