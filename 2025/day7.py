f7ebf130e89fd1c71edc255a27c41d3ec08170a8
Shaun Kenny <skenny@gmail.com>
2025-12-07 09:25:43 -0400

    day 7 part 1

--- 2025/day7.py
+++ 2025/day7.py
@@ -1,12 +1,35 @@
 def read_input():
     input = []
-    with open('input/day7-test.txt', 'r') as input_file:
+    with open('input/day7.txt', 'r') as input_file:
         input = [v for v in input_file.readlines()]
     return input
 
+def find_splitter_indices(input):
+    start = 0
+    while True:
+        start = input.find("^", start)
+        if start == -1: return
+        yield start
+        start += 1
+
 def part1(input):
-    
-    print("Part 1:")
+    num_splits = 0
+    beam_cols = set([input[0].index("S")])
+    for depth, level in enumerate(input):
+        #print(f"at depth {depth}, tracking beams at {beam_cols}; there have been {num_splits} splits...")
+        splitters = list(find_splitter_indices(level))
+        #print(f"\tfound splitters at {splitters}")
+        new_beam_cols = set()
+        for beam in beam_cols:
+            if beam in splitters:
+                #print(f"\tbeam {beam} splits!")
+                num_splits += 1
+                new_beam_cols.add(beam - 1)
+                new_beam_cols.add(beam + 1)
+            else:
+                new_beam_cols.add(beam)
+        beam_cols = new_beam_cols
+    print(f"Part 1: {num_splits}")
 
 def part2():
     print("Part 2:")
