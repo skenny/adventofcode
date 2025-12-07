2ca81f920774eb8f5f0f8d99be122d8f4b18ad1e
Shaun Kenny <skenny@gmail.com>
2025-12-07 09:11:21 -0400

    stubbed out day 7

---
+++ 2025/day7.py
@@ -0,0 +1,16 @@
+def read_input():
+    input = []
+    with open('input/day7-test.txt', 'r') as input_file:
+        input = [v for v in input_file.readlines()]
+    return input
+
+def part1(input):
+    
+    print("Part 1:")
+
+def part2():
+    print("Part 2:")
+
+input = read_input()
+part1(input)
+part2()
