f483cdd3aa108d7cc55e9f210a17b1301a1f47aa
Shaun Kenny <skenny@gmail.com>
2025-12-11 21:25:58 -0400

    day template

---
+++ 2025/day-template.py
@@ -0,0 +1,15 @@
+def read_input():
+    input = []
+    with open('input/dayN-test.txt', 'r') as input_file:
+        input = [v.strip() for v in input_file.readlines()]
+    return input
+
+def part1(input):
+    print("Part 1:")
+
+def part2(input):
+    print("Part 2:")
+
+input = read_input()
+part1(input)
+part2(input)
