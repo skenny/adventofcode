099b0a72a37f877658aa43750b1fc4b58077b74e
Shaun Kenny <skenny@gmail.com>
2025-12-13 18:49:08 -0400

    arg to control test input for read_input()

--- 2025/day-template.py
+++ 2025/day-template.py
@@ -1,6 +1,8 @@
-def read_input():
+def read_input(test):
+    file = "dayN"
+    if test: file += "-test"
     input = []
-    with open('input/dayN-test.txt', 'r') as input_file:
+    with open(f'input/{file}.txt', 'r') as input_file:
         input = [v.strip() for v in input_file.readlines()]
     return input
 
@@ -10,6 +12,6 @@ def part1(input):
 def part2(input):
     print("Part 2:")
 
-input = read_input()
+input = read_input(True)
 part1(input)
 part2(input)
