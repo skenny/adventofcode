ee2caf389be6cb9f89af7bccb9e68f9d34609b9b
Shaun Kenny <skenny@gmail.com>
2025-12-13 18:49:19 -0400

    day 9 stub

---
+++ 2025/day9.py
@@ -0,0 +1,17 @@
+def read_input(test):
+    file = "day9"
+    if test: file += "-test"
+    input = []
+    with open(f'input/{file}.txt', 'r') as input_file:
+        input = [v.strip() for v in input_file.readlines()]
+    return input
+
+def part1(input):
+    print("Part 1:")
+
+def part2(input):
+    print("Part 2:")
+
+input = read_input(True)
+part1(input)
+part2(input)
